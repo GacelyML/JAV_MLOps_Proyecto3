43ba68378adf49005314cab2db23b5d819ca6331
GacelyML <carvajal_brayan@javeriana.edu.co>
2024-05-06 15:06:57 -0500

    fix streamlit

--- src/front/main.py
+++ src/front/main.py
@@ -5,28 +5,38 @@ import requests
 import json
 
 st.title("Operaciones de Machine Learning")
-st.write("Proyecto 2 - Nivel 2")
+st.write("Proyecto 3 - Nivel 3")
 st.write("Orquestación, metricas y modelos")
 st.write("Por Juan Pablo Nieto, Nicolas Rojas y Brayan Carvajal")
 
 # Colección de variables.  
 st.write("### Registrar Dato")
-col1, col2, col3 = st.columns(3)
+col1, col2, col3, col4 = st.columns(4)
 
-Elevation = col1.number_input("Elevation", value=2792)
-Aspect = col1.number_input("Aspect", value=60)
-Slope = col1.number_input("Slope", value=10)
-Horizontal_Distance_To_Hydrology = col1.number_input("Horizontal_Distance_To_Hydrology", value=543)
+race = col1.text_input('race', value='Caucasian')
+gender = col2.text_input("gender", value='Male')
+age = col3.number_input("age", value=75)
+admission_type_id = col4.text_input("admission_type_id", value='Emergency')
 
-Vertical_Distance_To_Hydrology = col2.number_input("Vertical_Distance_To_Hydrology", value=8)
-Horizontal_Distance_To_Roadways = col2.number_input("Horizontal_Distance_To_Roadways", value=1679)
-Hillshade_9am = col2.number_input("Hillshade_9am", value=228)
-Hillshade_Noon = col2.number_input("Hillshade_Noon", value=218)
+discharge_disposition_id = col1.text_input('discharge_disposition_id', value='Discharged to Home')
+admission_source_id = col2.text_input("admission_source_id", value='Emergency')
+time_in_hospital = col3.number_input("time_in_hospital", value=2)
+num_lab_procedures = col4.number_input("num_lab_procedures", value=36)
 
-Hillshade_3pm = col3.number_input("Hillshade_3pm", value=122)
-Horizontal_Distance_To_Fire_Points = col3.number_input("Horizontal_Distance_To_Fire_Points", value=1024)
-Wilderness_Area = col3.text_input("Wilderness_Area", value="Rawah")
-Soil_Type = col3.text_input("Soil_Type", value="C4744")
+num_procedures = col1.number_input('num_procedures', value=0)
+num_medications = col2.number_input("num_medications", value=8)
+number_outpatient = col3.number_input("number_outpatient", value=0)
+number_emergency = col4.number_input("number_emergency", value=0)
+
+number_inpatient = col1.number_input('number_inpatient', value=1)
+number_diagnoses = col2.number_input("number_diagnoses", value=9)
+max_glu_serum = col3.text_input("max_glu_serum", value='None')
+A1Cresult = col4.text_input("A1Cresult", value='None')
+
+examide = col1.text_input('examide', value='No')
+citoglipton = col2.text_input("citoglipton", value='No')
+change = col3.text_input("change", value='Ch')
+diabetesMed = col4.text_input("diabetesMed", value='Yes')
 
 # Ejecutar la instrucción indicada. 
 click = st.button("Predecir")
@@ -43,18 +53,26 @@ if click or st.session_state.load_state:
         # Armar JSON con estructura de datos como lo necesita la API a consumir. 
         # Agrupa las variables en un diccionario
         datos = {
-            "Elevation" :  Elevation,
-            "Aspect" : Aspect,
-            "Slope" : Slope,
-            "Horizontal_Distance_To_Hydrology" : Horizontal_Distance_To_Hydrology,
-            "Vertical_Distance_To_Hydrology" : Vertical_Distance_To_Hydrology,
-            "Horizontal_Distance_To_Roadways" : Horizontal_Distance_To_Roadways,
-            "Hillshade_9am" : Hillshade_9am,
-            "Hillshade_Noon" : Hillshade_Noon,
-            "Hillshade_3pm" : Hillshade_3pm,
-            "Horizontal_Distance_To_Fire_Points" : Horizontal_Distance_To_Fire_Points,
-            "Wilderness_Area" : Wilderness_Area,
-            "Soil_Type" : Soil_Type
+            "race" :  race,
+            "gender" : gender,
+            "age" : age,
+            "admission_type_id" : admission_type_id,
+            "discharge_disposition_id" : discharge_disposition_id,
+            "admission_source_id" : admission_source_id,
+            "time_in_hospital" : time_in_hospital,
+            "num_lab_procedures" : num_lab_procedures,
+            "num_procedures" : num_procedures,
+            "num_medications" : num_medications,
+            "number_outpatient" : number_outpatient,
+            "number_emergency" : number_emergency,
+            "number_inpatient" : number_inpatient,
+            "number_diagnoses" : number_diagnoses,
+            "max_glu_serum" : max_glu_serum,
+            "A1Cresult" : A1Cresult,
+            "examide" : examide,
+            "citoglipton" : citoglipton,
+            "change" : change,
+            "diabetesMed" : diabetesMed
         }
 
         # Convierte el diccionario a una cadena JSON
@@ -62,7 +80,7 @@ if click or st.session_state.load_state:
         # st.json(json_str)
 
         # Request al API generado para calcular predicción sobre dato. 
-        url = 'http://10.43.101.154:8086/predict/'
+        url = 'http://fast_api:8086/predict/'
 
         response = requests.post(url, data=json_str)
 
@@ -70,6 +88,3 @@ if click or st.session_state.load_state:
         st.metric("resultado", value=response.text )
     except Exception as e: 
         st.metric("Falla:", value=e)
-
-#streamlit run main.py
-#Esta API debe estar expuesta en el puerto 8501.
